f735ac9c2fd689310c76ba338bf8da6111101a82
felisito1999 <felejunier@hotmail.com>
2020-02-08 01:30:42 -0400

    Creacion primeros documentos

---
+++ Query DataBase.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE NationalBasketballAssociation_DB;
+
+USE NationalBasketballAssociation_DB;
+
+CREATE TABLE Sexos
+(
+	CodigoSexo INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Sexos PRIMARY KEY,
+	NombreSexo VARCHAR(20) NOT NULL
+);
+GO
+CREATE TABLE Paises
+(
+	CodigoPais INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Paises PRIMARY KEY,
+	ISO_Pais CHAR(2) NOT NULL,
+	NombrePais VARCHAR(50) NOT NULL 
+);
+CREATE TABLE EstadosPaises 
+(
+	CodigoEstado INT NOT NULL CONSTRAINT PK_EstadosPaises PRIMARY KEY,
+	NombreEstado VARCHAR(50) NOT NULL,
+	CodigoPais INT NOT NULL CONSTRAINT FK_PaisesEstados FOREIGN KEY REFERENCES Paises(CodigoPais)
+)
+GO
+CREATE TABLE Estados 
+(
+	CodigoEstado INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Estados PRIMARY KEY,
+	DescripcionEstado VARCHAR(30) NOT NULL
+);
+GO
+CREATE TABLE StatsAllTimePlayer
+(
+	CodigoStats INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_StatsAllTimePlayer PRIMARY KEY,
+	PuntosTotales INT NOT NULL,
+
+)
+GO
+CREATE TABLE Jugador
+(
+	CodigoJugador INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Jugadores PRIMARY KEY,
+	NombreJugador VARCHAR(50) NOT NULL,
+	ApellidoJugador VARCHAR(50) NOT NULL,
+	PaisJugador INT CONSTRAINT FK_Paises_Jugadores FOREIGN KEY REFERENCES Paises(CodigoPais),
+	FechaNacimiento DATE, 
+	StatsAllTimePlayer INT NOT NULL CONSTRAINT FK_StatsAllTimePlayer 
+)
+GO
+CREATE TABLE StatsAllTimePlayer
+(
+	CodigoStats INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_StatsAllTimePlayer PRIMARY KEY,
+	PuntosTotales INT NOT NULL,
+
+)
+GO 
+CREATE TABLE Equipos
+(
+	CodigoEquipo INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Equipos PRIMARY KEY,
+	NombreEquipo VARCHAR(80) NOT NULL, 
+
+)
